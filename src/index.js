615d86579e9fbd068d0f6337b2147d3db322d46b
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-23 04:11:49 +0200

    feat: add query-client provider to the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {QueryClientProvider } from 'react-query';
+
 import './index.css';
 import App from './App';
+import {queryClient} from './fetch/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
