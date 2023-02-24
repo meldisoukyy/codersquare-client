e38600214057bfb714da2aa3173971ca5d0ffa94
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-25 00:09:45 +0200

    feat: add fetchData function

--- src/fetch/client.js
+++ src/fetch/client.js
@@ -1,15 +1,24 @@
 import { QueryClient } from "react-query";
 
+import { ENDPOINTS } from "./endpoints";
+
+const API_HOST = 'http://localhost:3001'
+const AUTH = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5MDA5YjM0LTEwMzktNDNjZC1iNmUzLWQwM2E1OTU0MGU1ZiIsImlhdCI6MTY3NzExMjEwMiwiZXhwIjoxNjc5NzA0MTAyfQ.BACwWwU_46o9n4EgBEOXnSHLtyLyIsbpZSFfwzsh1yU';
+
 export const queryClient = new QueryClient();
 
-export async function getPosts() {
-  const response = await fetch('http://localhost:3001/posts', {
-    method: 'GET',
+async function fetchData(endpoint) {
+  const { url, method } = endpoint;
+
+  const response = await fetch(`${API_HOST}${url}`, {
+    method: method,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5MDA5YjM0LTEwMzktNDNjZC1iNmUzLWQwM2E1OTU0MGU1ZiIsImlhdCI6MTY3NzExMjEwMiwiZXhwIjoxNjc5NzA0MTAyfQ.BACwWwU_46o9n4EgBEOXnSHLtyLyIsbpZSFfwzsh1yU'
+      'Authorization': AUTH
     }
   })
 
-  return await response.json()
+  return await response.json();
 }
+
+export const getPosts = () => fetchData(ENDPOINTS.listPosts)
