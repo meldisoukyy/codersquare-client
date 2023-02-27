d2b9bb9106f634574e0cc524dc9409e7e451df55
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-28 00:38:23 +0200

    refact: change fetch/client to fetch/index and getData to callEndPoint

---
+++ src/fetch/index.js
@@ -0,0 +1,27 @@
+import { QueryClient } from "react-query";
+
+import { ENDPOINTS } from "./endpoints";
+
+const API_HOST = 'http://localhost:3001'
+const AUTH = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5MDA5YjM0LTEwMzktNDNjZC1iNmUzLWQwM2E1OTU0MGU1ZiIsImlhdCI6MTY3NzExMjEwMiwiZXhwIjoxNjc5NzA0MTAyfQ.BACwWwU_46o9n4EgBEOXnSHLtyLyIsbpZSFfwzsh1yU';
+
+export const queryClient = new QueryClient();
+
+export async function callEndPoint(endpoint, request) {
+  const { url, method } = endpoint;
+  const requestBody = request? JSON.stringify(request): undefined;
+
+  const response = await fetch(`${API_HOST}${url}`, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': AUTH
+    },
+    body: requestBody
+  })
+
+  return await response.json();
+}
+
+// POSTS
+export const getPosts = () => callEndPoint(ENDPOINTS.listPosts)
