4a5519dfb806ec09f7ae15bf0e8f48971e3093a1
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-28 00:33:17 +0200

    feat(signup): add signup to registeration page

---
+++ src/pages/registration.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import { ENDPOINTS } from '../fetch/endpoints';
+import { fetchData } from '../fetch/client'
+
+const Signup = () => {
+  const [user, setUser] = useState({
+    username: null,
+    email: null,
+    firstName: null,
+    lastName: null,
+    password: null
+  })
+
+  const changeUser = (e, id) => {
+    const newUser = {...user}
+    newUser[id] = e.target.value;
+    setUser(newUser)
+  }
+
+  const signup = async (e) => {
+    e.preventDefault();
+    const response = await fetchData(ENDPOINTS.signup, user);
+    console.log(response);
+  }
+
+  return (
+    <form onSubmit={signup}>
+      <input
+        key={'username'}
+        placeholder={'username'}
+        type={'text'}
+        required
+        onChange={e => changeUser(e, 'username')}
+      ></input>
+
+      <input
+        key={'email'}
+        placeholder={'email'}
+        type={'text'}
+        required
+        onChange={e => changeUser(e, 'email')}
+      ></input>
+
+      <input
+        key={'firstName'}
+        placeholder={'firstName'}
+        type={'text'}
+        required
+        onChange={e => changeUser(e, 'firstName')}
+      ></input>
+
+      <input
+        key={'lastName'}
+        placeholder={'lastName'}
+        type={'text'}
+        required
+        onChange={e => changeUser(e, 'lastName')}
+      ></input>
+
+      <input
+        key={'password'}
+        placeholder={'password'}
+        type={'password'}
+        required
+        onChange={e => changeUser(e, 'password')}
+      ></input>
+
+      <button type={'submit'}>Submit</button>
+    </form>
+  );
+}
+
+export const Registration = () => {
+  return (
+    <Signup></Signup>
+  );
+}
