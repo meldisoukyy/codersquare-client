f8c50c2809b745b7cc29349c8e860649bdaab9da
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-28 00:54:23 +0200

    refact: change fetch/client inmport to fetch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {QueryClientProvider } from 'react-query';
 
 import './index.css';
 import App from './App';
-import {queryClient} from './fetch/client';
+import {queryClient} from './fetch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
