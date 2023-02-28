d0cb9b92d98d91c7b1eaf780e6a86e8bc64c26c4
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-28 03:50:08 +0200

    feat(add post): add addPost functionality

--- src/pages/list_posts.js
+++ src/pages/list_posts.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
-import { getPosts } from '../fetch/';
+import { getPosts, addPost as addPostRequest } from '../fetch/';
 import { useTitle } from '../hooks/title';
 
 export const ListPosts = () => {
   useTitle('Home');
 
+  const [post, setPost] = useState({
+    title: null,
+    body: null
+  });
   const { data, error, isLoading } = useQuery(['listPosts'], getPosts);
-  const { response } = data? data: {response:null};
+  const { response } = data ? data : { response: null };
 
   if (isLoading) return <h1>Loading... </h1>
   if (error) return <h1>There is an error.</h1>
 
+  const addPost = async (e) => {
+    e.preventDefault();
+    const { response, status } = await addPostRequest(post);
+    if (status === 200)
+      window.location.reload(true)
+    else
+      console.log(status + ' ' + response.msg);
+  }
+
   return (
     <div>
+      <div>ADD POST:</div>
+      <form onSubmit={addPost}>
+        <input
+          key={'title'}
+          placeholder={'title'}
+          type={'text'}
+          required
+          onChange={e => setPost({ ...post, title: e.target.value })}
+        ></input>
+        <input
+          key={'body'}
+          placeholder={'body'}
+          type={'text'}
+          required
+          onChange={e => setPost({ ...post, body: e.target.value })}
+        ></input>
+        <button type='submit'>Add Post</button>
+      </form>
       <div>POSTS:</div>
       <ul>
         {
