b981ad6e9f0cd9f4bf03207e11c76089aadcbd7e
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-02-28 02:55:56 +0200

    feat(list_posts): update page with new callEndPoint function

--- src/pages/list_posts.js
+++ src/pages/list_posts.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 
-import { getPosts } from '../fetch/client';
+import { getPosts } from '../fetch/';
 
 export const ListPosts = () => {
   const { data, error, isLoading } = useQuery(['listPosts'], getPosts);
+  const { response } = data? data: {response:null};
 
   if (isLoading) return <h1>Loading... </h1>
   if (error) return <h1>There is an error.</h1>
 
   return (
     <div>
-      Posts:
       <ul>
         {
-          !!data?.posts && data.posts.map((post, i) => {
+          !!response?.posts && response.posts.map((post, i) => {
             return <li key={i}>{JSON.stringify(post.title)}</li>
-          }
-          )
+          })
         }
       </ul>
     </div>
