b3da1164c10020b0025b4b4d037b524e36e2a3ed
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-02 01:51:08 +0200

    feat(homepage): update page with PostsList component

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,70 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
 
-import { getPosts, addPost as addPostRequest, deletePost} from '../fetch';
+import { getPosts } from '../fetch';
 import { useTitle } from '../hooks/title';
+import { PostsList } from '../components/posts-list';
+import { Navbar } from '../components/navbar';
 
 export const Homepage = () => {
   useTitle('Home');
 
-  const [post, setPost] = useState({
-    title: null,
-    body: null
-  });
-  const  {data} = useQuery(['listPosts'], getPosts);
-  const { response } = data ? data : { response: null };
-
-  const addPost = async (e) => {
-    e.preventDefault();
-    const { response, status } = await addPostRequest(post);
-    if (status === 200)
-      window.location.reload(true)
-    else
-      console.log(status + ' ' + response.msg);
-  }
-
-  const handleDeletePost = async (postId) => {
-    const {response, status} = await deletePost(postId);
-    if (status === 200)
-      window.location.reload(true);
-    else
-      console.log(status + ' ' + response.msg);
-  }
+  const { data: posts, refetch } = useQuery(['listPosts'], getPosts);
 
   return (
     <div>
-      <div>ADD POST:</div>
-      <form onSubmit={addPost}>
-        <input
-          key={'title'}
-          placeholder={'title'}
-          type={'text'}
-          required
-          onChange={e => setPost({ ...post, title: e.target.value })}
-        ></input>
-        <input
-          key={'body'}
-          placeholder={'body'}
-          type={'text'}
-          required
-          onChange={e => setPost({ ...post, body: e.target.value })}
-        ></input>
-        <button type='submit'>Add Post</button>
-      </form>
-      <div>POSTS:</div>
-      <ul>
-        {
-          !!response?.posts && response.posts.map((post, i) => {
-            return <li key={i}>
-              <Link to={`/post/${post.id}`}>
-                {JSON.stringify(post.title)}
-              </Link>
-              <button onClick={() => handleDeletePost(post.id)}>Delete</button>
-            </li>
-          })
-        }
-      </ul>
+      <Navbar refetchPosts={refetch}></Navbar>
+      <div className='under-navbar container'>
+        <PostsList posts={posts} refetch={refetch}></PostsList>
+      </div>
     </div >
   );
 }
