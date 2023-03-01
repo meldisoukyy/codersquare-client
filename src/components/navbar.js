45823f175814b49b0ac7b423d0f9a1e8be9182a9
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-02 01:52:27 +0200

    feat(navbar): add modal to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AddPost } from './add-post';
+import { Modal } from './modal';
 import './style/navbar.scss';
 
-export const Navbar = () => {
+export const Navbar = ({ refetchPosts }) => {
+  const location = useLocation();
   const navigator = useNavigate();
+  const [modal, setModal] = useState(false);
+  const currentUser = localStorage.getItem('CURRENT_USER');
+
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem('JWT');
+    localStorage.removeItem('CURRENT_USER');
+    navigator('/user/login');
+  }, [navigator])
 
   return (
     <div className="navbar">
       <div className="container">
+        {!!modal &&
+          <Modal
+            setModal={setModal}
+            content={
+              location.pathname === '/' ? <AddPost setModal={setModal} refetch={refetchPosts}></AddPost> : 'New Comment'
+            }
+          ></Modal>
+        }
+
         <div className="logo" onClick={() => navigator('/')}>
-          <img src={process.env.PUBLIC_URL+"/imgs/logo.svg"} alt=""></img>
+          <img src={process.env.PUBLIC_URL + "/imgs/logo.svg"} alt=""></img>
         </div>
 
         <ul className="options">
-          <li className="option wrap-text new-post">
+          <li className="option wrap-text new-post" onClick={() => setModal(!modal)}>
             <i className="fa-solid fa-pen-to-square"></i>
-            New Post
+            {location.pathname === '/' && 'New Post'}
+            {location.pathname.match('/post') && 'New Comment'}
           </li>
 
           <li className="option wrap-text username">
-            meldisoukyy (8)
+            {currentUser}
           </li>
 
-          <li className="option wrap-text sign-out">
+          <li className="option wrap-text sign-out" onClick={handleLogout}>
             Sign out
           </li>
         </ul>
 
       </div>
-    </div>
+    </div >
   );
 }
+
