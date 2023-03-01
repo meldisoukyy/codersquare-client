0d3880b1f52404d50babc5d3e7cfabfea743b032
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-01 17:33:50 +0200

    feat(errors handling): add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style/404.scss';
+import ERROR_SVG from './imgs/404-Error.svg';
+import { useTitle } from '../hooks/title';
+
+export const NotFound = () => {
+  useTitle('404');
+  const navigate = useNavigate();
+
+  return (
+    <div className='container'>
+      <img
+        className='not-found'
+        src={ERROR_SVG}
+        alt="404 - page not found!"
+      ></img>
+      <div className='main-text'>Oops! Page not found</div>
+      <div className='sub-text'>The page you are trying to access does not exist or has been moved.</div>
+      <div className='sub-text'>Try going back to our homepage. </div>
+
+      <button
+        className={"go-back logo-font main-text"}
+        onClick={() => navigate('/')}
+      >Go back to Codersquare</button>
+    </div>
+  );
+}
