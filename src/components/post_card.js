18fd47309493b2a33196a9bfcbe87b8f0adea8cd
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-01 21:14:44 +0200

    feat(post_card): update post card with like button, likes counter, and comments counter

--- src/components/post_card.js
+++ src/components/post_card.js
@@ -1,40 +1,72 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
+import formatDistance from 'date-fns/formatDistance';
 
 import './style/post_card.scss'
+import { addLike as addLikeRequest, deleteLike as deleteLikeRequest, getComments, getLikes } from '../fetch';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
-export const PostCard = ({ postId, post }) => {
-  const [isClicked, setClicked] = useState(false)
+export const PostCard = ({ post }) => {
+  const navigator = useNavigate();
+  const { isLiked, likesCount, refetch } = useLikesInfo(post.id);
+  const { commentsCount } = useCommentInfo(post.id);
+
+  const viewPost = useCallback(async () => {
+    navigator('/post/'+post.id);
+  }, [navigator, post.id])
+
+  const toggleLike = useCallback(async () => {
+    if (!!isLiked) {
+      await deleteLikeRequest(post.id);
+    } else {
+      await addLikeRequest({ "postId": post.id });
+    }
+    refetch();
+  }, [refetch, isLiked, post.id])
+
   return (
-    <div className={isClicked?"card on-click":"card"} onClick={e => setClicked(!isClicked)}>
-      <div className="like-button">
-        <i className="like-icon fa-regular fa-heart"></i>
+    <div className={"card"} onClick={viewPost}>
+      <div className="like-button" onClick={toggleLike}>
+        <i className={`like-icon ${isLiked ? "fa-solid" : "fa-regular"} fa-heart`}></i>
       </div>
 
       <div className="title wrap-text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
+        {post.title}
       </div>
 
       <div className="meta-data">
-        <div className="likes">11 likes</div>
+        <div className="likes wrap-text">{likesCount} likes</div>
         <div className="spliter">|</div>
         <div className="username wrap-text">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
+          {post.postAuthor.username}
         </div>
         <div className="spliter">|</div>
-        <div className="time">3 hours ago</div>
+        <div className="comments wrap-text">
+          {commentsCount} Comments
+        </div>
+        <div className="spliter">|</div>
+        <div className="time wrap-text">
+          {formatDistance(new Date(post.createdAt), Date.now(), { addSuffix: true })}
+        </div>
       </div>
-      {!!isClicked && <div className="body">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis dicta sunt voluptatibus id qui libero quae
-      </div>}
     </div>
   );
+}
+
+const useLikesInfo = (postId) => {
+  const { data, refetch } = useQuery(['getLikes', postId], () => getLikes(postId));
+
+return {
+  likesCount: data?.count,
+  isLiked: data?.isLiked,
+  refetch
+}
+}
+
+const useCommentInfo = (postId) => {
+  const { data } = useQuery(['getComments', postId], () => getComments(postId));
+
+  return {
+    commentsCount: data?.comments.length
+  }
 }
