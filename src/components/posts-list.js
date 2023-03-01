6fe89252e179f2e129aa8120ef4eb61ee0af0dbe
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-02 01:49:51 +0200

    refact: eslint edits

--- src/components/posts-list.js
+++ src/components/posts-list.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { PostCard } from './post_card';
 import './style/list.scss';
 
-export const PostsList = ({posts, refetch}) => {
+export const PostsList = ({ posts, refetch }) => {
   return (
     <ul className='list'>
       {
         !!posts?.posts && posts.posts.map((post, i) => {
-          return <li key={i} className='list-item'>
-            <PostCard post={post} refetch={refetch}></PostCard>
-          </li>
+          return (
+            <li key={i} className='list-item'>
+              <PostCard post={post} refetch={refetch}></PostCard>
+            </li>
+          )
         })
       }
     </ul>
