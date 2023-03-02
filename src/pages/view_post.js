35aa93b0d2e8bf6a6d5997c46e5cf80ea8f97c35
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-02 03:49:21 +0200

    feat(view post): add post, and comments components

--- src/pages/view_post.js
+++ src/pages/view_post.js
@@ -1,64 +1,31 @@
-import React, { useState } from 'react';
+import React, { useCallback} from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
-import { addComment as addCommentRequest, deleteComment, getComments, viewPost } from '../fetch';
+import { deleteComment, getComments, viewPost } from '../fetch';
+import { useTitle } from '../hooks/title';
+import { CommentsList } from '../components/comments-list';
+import { Navbar } from '../components/navbar';
+import { PostCard } from '../components/post_card';
 
 export const ViewPost = () => {
+  useTitle('Post')
   const { id } = useParams();
-  const postsQuery = useQuery(['viewPost', id], () => viewPost(id));
-  const commentsQuery = useQuery([getComments, id], () => getComments(id));
-  const [comment, setComment] = useState('');
-  const postsData = postsQuery.data? postsQuery.data : undefined;
-  const commentsData = commentsQuery.data? commentsQuery.data : und;
+  const { data: post, isLoading } = useQuery(['viewPost'], () => viewPost(id));
+  const { data: comments, refetch } = useQuery(['getComments'], () => getComments(id));
 
-  const addComment = async (e) => {
-    e.preventDefault();
-    const { status, response } = await addCommentRequest({
-      postId: id,
-      body: comment
-    })
-
-    if (status === 200)
-      window.location.reload(true);
-    else
-      console.log(status + ' : ' + response);
-  }
-
-  const handleDeleteComment = async (id) => {
-    const { status, response } = await deleteComment(id)
-
-    if (status === 200)
-      window.location.reload(true);
-    else
-      console.log(status + ' : ' + response);
-  }
+  const handleDeleteComment = useCallback(async (id) => {
+    await deleteComment(id);
+    refetch();
+  }, [refetch])
 
   return (
     <>
-      <h2 key={'postId'}>POST ID : {id}</h2>
-      <h3 key={'postData'}>{JSON.stringify(postsData)}</h3>
-      <hr></hr>
-      <h2 key={'comments'}>POST COMMENTS: </h2>
-      <ul key={'postComments'}>{
-        !!commentsData?.response.comments &&
-        commentsData.response.comments.map((comment, id) => {
-          return <li key={id}>
-            {comment.body}
-            <button onClick={() => {handleDeleteComment(comment.id)}}>Delete Comment!</button>
-          </li>
-        })
-      }</ul>
-      <hr></hr>
-      <h2 key={'addComment'}>Add Comment:</h2>
-      <form onSubmit={addComment}>
-        <input
-          placeholder='comment'
-          type={'text'}
-          required
-          onChange={e => setComment(e.target.value)}
-        ></input>
-        <button type='submit'>Add Comment!</button>
-      </form>
+      <Navbar refetchComments={refetch} postId={id}></Navbar>
+      <div className='container under-navbar'>
+        {!isLoading && <PostCard post={post} postId={id}></PostCard>}
+        <hr></hr>
+        <CommentsList comments={comments} handleDeleteComment={handleDeleteComment} currentUser={localStorage.getItem('CURRENT_USER')}></CommentsList>
+      </div>
     </>
   );
 }
