606a995abbfbd0fe25a0a8898798ba7e50d59d68
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2023-03-02 04:37:40 +0200

    feat: update fetching functions

--- src/fetch/index.js
+++ src/fetch/index.js
@@ -8,7 +8,7 @@ export const queryClient = new QueryClient();
 
 async function callEndPoint(endpoint, request) {
   const { url, method } = endpoint;
-  const requestBody = request? JSON.stringify(request): undefined;
+  const requestBody = request ? JSON.stringify(request) : undefined;
 
   const response = await fetch(`${API_HOST}${url}`, {
     method: method,
@@ -22,28 +22,62 @@ async function callEndPoint(endpoint, request) {
   const status = response.status;
 
   const isJson = response.headers.get('content-type')?.includes('application/json');
-  const jsonResponse = isJson? await response.json() : {}
+  const jsonResponse = isJson ? await response.json() : {}
 
-  return {status: status, response: jsonResponse};
+  return { status: status, response: jsonResponse };
 }
 
 // USERS
 export const signup = (user) => callEndPoint(ENDPOINTS.signup, user);
-export const login  = (user) => callEndPoint(ENDPOINTS.login, user);
+export const login = (user) => callEndPoint(ENDPOINTS.login, user);
 export const logout = () => localStorage.removeItem('JWT');
 
 // POSTS
-export const getPosts = () => callEndPoint(ENDPOINTS.listPosts);
-export const addPost = (post) => callEndPoint(ENDPOINTS.addPost, post);
-export const viewPost = (id) => callEndPoint(handleParam(id, ENDPOINTS.viewPost));
+export const getPosts = async () => {
+  const { status, response } = await callEndPoint(ENDPOINTS.listPosts);
+  if (status === 200)
+    return response;
+  return console.log("SHIT!: getPosts");
+}
+export const addPost = async (post) => {
+  const { status, response } = await callEndPoint(ENDPOINTS.addPost, post);
+  if (status === 200)
+    return response;
+  return console.log("SHIT!: addPost");
+}
+export const viewPost = async (id) => {
+  const { response, status } = await callEndPoint(handleParam(id, ENDPOINTS.viewPost));
+  if (status === 200)
+    return response;
+  return console.log("SHIT!: viewPost");
+}
+
 export const deletePost = (id) => callEndPoint(handleParam(id, ENDPOINTS.deletePost));
 
 // Comments
-export const getComments = (postId) => callEndPoint(handleParam(postId, ENDPOINTS.getComments));
-export const addComment = (comment) => callEndPoint(ENDPOINTS.addComment, comment);
+export const getComments = async (postId) => {
+  const { status, response } = await callEndPoint(handleParam(postId, ENDPOINTS.getComments));
+  if (status === 200)
+    return response;
+  return console.log("SHIT!: getComments");
+}
+
+export const addComment = async (comment) => {
+  const { status, response } = await callEndPoint(ENDPOINTS.addComment, comment);
+  if (status === 200)
+    return response;
+  return console.log("SHIT!: addComment");
+}
+
 export const deleteComment = (id) => callEndPoint(handleParam(id, ENDPOINTS.deleteComment));
 
 // LIKES
-export const getLikes = (postId) => callEndPoint(handleParam(postId, ENDPOINTS.getLikes));
+export const getLikes = async (postId) => {
+  const { status, response } = await callEndPoint(handleParam(postId, ENDPOINTS.getLikes));
+  if (status === 200)
+    return response
+  return console.error("SHIT!");
+}
+
 export const addLike = (id) => callEndPoint(ENDPOINTS.addLike, id);
 export const deleteLike = (id) => callEndPoint(handleParam(id, ENDPOINTS.deleteLike));
